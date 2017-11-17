1355f381d7634cdc3e9fb17d8c4c477bd54851f1
intarasuwan <mild_suttida@hotmail.com>
2017-11-17 11:22:59 +0700

    Successfuly information data

--- src/myFirstJAVA.java
+++ src/myFirstJAVA.java
@@ -1,6 +1,9 @@
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm Suttida");
+        System.out.println( "Information System");
+        System.out.println("Faculty of Managment Technololqy");
+        System.out.println("RMUTSV");
+
     }
 }
- 

9abc1d8f910c78118b4fbebaa2018dad2989513c
suttida Intarasuwan <mild_suttida@hotmail.com>
2017-11-24 10:58:53 +0700

    Test Input

---
+++ src/ooplab2/Testlnput.java
@@ -0,0 +1,21 @@
+package ooplab2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Testlnput {
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+    public class TestInput {
+        //BufferedReader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("whath is your name?:");
+        string name = reader.readLine();
+        System.out.print("Your name is"+name);
+
+
+    }//maim
+}//class
+

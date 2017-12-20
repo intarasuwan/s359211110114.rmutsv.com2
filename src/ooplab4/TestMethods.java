86cc71c73cec5eea1db1f7c9c6dc82c9d7cb8e9a
LAB_COM-MT_1 <mild_suttida@hotmail.com>
2017-12-20 16:48:36 +0700

    TestNestedLoop

---
+++ src/ooplab4/TestMethods.java
@@ -0,0 +1,11 @@
+package ooplab4;
+
+public class TestMethods {
+    //global variable
+    private static int number;
+    public static void main (String [] args) {
+        // local variable
+        int x=100;
+        System.out.println(x);
+    }//main
+}//class

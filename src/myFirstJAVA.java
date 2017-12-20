23e4253d26bbfc9decd9abc9871750e3c241fbc3
LAB_COM-MT_1 <mild_suttida@hotmail.com>
2017-12-20 17:39:09 +0700

    TestNestedLoop oopleb4

--- src/myFirstJAVA.java
+++
@@ -1,9 +0,0 @@
-public class myFirstJAVA {
-    public static void main(String[] args) {
-        System.out.println("Hello Word, I'm Suttida");
-        System.out.println( "Information System");
-        System.out.println("Faculty of Managment Technololqy");
-        System.out.println("RMUTSV");
-
-    }
-}

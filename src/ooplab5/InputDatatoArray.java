87e15f504c3c584c323b7878dd63beab858c7f7c
LAB_COM-MT_1 <mild_suttida@hotmail.com>
2017-12-22 09:54:22 +0700

    InputtoData Array

---
+++ src/ooplab5/InputDatatoArray.java
@@ -0,0 +1,22 @@
+package ooplab5;
+
+public class InputDatatoArray {
+    private static  int MAX = 5;
+    public  static void  main (String[] args) {
+        int number[] = new int[MAX];
+        //System.out.println(number.length);
+        //for
+        number = inputData(number);
+      ShowData (number)
+    }//main
+    private  static void [] showDataEnchance (int[]number){
+        System.out.println("Data in Array");
+        for (int val.number)
+            System.out.print(val +"");
+    }
+
+    private  static void [] showData (int[]number){
+        System.out.println("Data in Array");
+        for (int i=0;i<number.length;i++)
+        System.out.print(number[i}+"");
+    }
